9a328918a8fc94a9490059f4e5e2e57f279bff2e
sindhusugi <30830093+sindhusugi@users.noreply.github.com>
2018-01-21 09:52:13 +0530

    Update palindrome.c

--- palindrome.c
+++ palindrome.c
@@ -5,28 +5,15 @@ int n, reverInt= 0, remaind, originalint;
 printf("Enter an integer");    
 scanf("%d", &n);   
 originalint= n;  
- while( n!=0 )
-    
-{
-        
-remainder = n%10;
-        
-reverint=reverint*10 + remaind;
-       
- n /= 10;
-    
-}
-
-       
-if (originalint== reverint)
-        
-printf("%d is a palindrome", originalint);
-    
-else
-        
-printf("%d is not a palindrome", originalint);
-    
-   
+ while( n!=0 )   
+{        
+remainder = n%10;        
+reverint=reverint*10 + remaind;       
+ n /= 10;   
+}       
+if (originalint== reverint)        
+printf("%d is a palindrome", originalint);   
+else        
+printf("%d is not a palindrome", originalint);  
 return 0;
-
 }
