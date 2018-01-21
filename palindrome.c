3290198497c3a73884f330ea628836b11e0c8a97
sindhusugi <30830093+sindhusugi@users.noreply.github.com>
2018-01-21 09:50:03 +0530

    Create palindrome.c

---
+++ palindrome.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int main()
+{  
+int n, reverInt= 0, remaind, originalint;   
+printf("Enter an integer");    
+scanf("%d", &n);   
+originalint= n;  
+ while( n!=0 )
+    
+{
+        
+remainder = n%10;
+        
+reverint=reverint*10 + remaind;
+       
+ n /= 10;
+    
+}
+
+       
+if (originalint== reverint)
+        
+printf("%d is a palindrome", originalint);
+    
+else
+        
+printf("%d is not a palindrome", originalint);
+    
+   
+return 0;
+
+}
