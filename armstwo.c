9c3cd391ef211919edf4c69136e74ea356d40b78
sindhusugi <30830093+sindhusugi@users.noreply.github.com>
2018-01-21 10:56:29 +0530

    Update armstwo.c

--- armstwo.c
+++ armstwo.c
@@ -15,8 +15,7 @@ temp1 /= 10;
 } 
 while (temp2 != 0)       
 {            
-remainder = temp2 % 10;            
-result += pow(remainder, n);           
+remainder = temp2 % 10;           
 temp2 /= 10;        
 }       
 if (result == i) 
