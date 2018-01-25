f22c70ef9db5d2dad56f529dbda075b7305c3717
sindhusugi <30830093+sindhusugi@users.noreply.github.com>
2018-01-25 16:00:16 +0530

    Create countlines.c

---
+++ countlines.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void main()
+{
+   char str[100];
+   int i = 0, l = 0, f = 1;
+   clrscr();
+   printf("Enter any string\n");
+   scanf("%d"str);
+   for(i = 0; str[i] !='\0'; i++)
+   {
+      l = l + 1;
+   }
+   printf("The number of characters in the string are %d\n", l);
+   for(i = 0; i <= l-1; i++)
+   {
+   if(str[i] == ' ')
+   {
+   f = f + 1;
+   }
+   }
+   printf("The number of words in the string are %d", f);
+   getch();
+}
