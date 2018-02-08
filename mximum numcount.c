33624ebffa74bcfc586ddc82e9c281d9f7a2ea0a
sindhusugi <30830093+sindhusugi@users.noreply.github.com>
2018-02-08 09:37:07 +0000

    Update mximum numcount.c

--- mximum numcount.c
+++ mximum numcount.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-int n,temp,i,j;
+int temp,i,j,a[10];
 printf(" enter the element \n");
 for(i=0;i<10;i++)
 {
@@ -10,7 +10,7 @@ printf("%d \t",a[i]);
 }
 for(i=0;i<10;i++)
 {
-for(j=0;j<10;j++)
+for(j=i+1;j<10;j++)
 {
 if(a[i]>a[j])
 {
@@ -22,8 +22,10 @@ a[j]=temp;
 }
 for(i=0;i<10;i++)
 {
-if(a[i]>a[j])
-}
+if(a[i]>a[i+1])
+{
 printf("\n %d maximun",a[i]);
+}
+}
 return 0;
 }
