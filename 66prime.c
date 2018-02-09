d5ab9cb9d410e88f8960d0fcbaae988cea810df7
sindhusugi <30830093+sindhusugi@users.noreply.github.com>
2018-02-09 09:34:28 +0000

    Rename prime.c to 66prime.c

---
+++ 66prime.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+int i, num, p = 0;
+printf("Please enter a number: \n");
+scanf("%d", &num);
+for(i=1; i<=num; i++)
+{
+if(num%i==0)
+{
+ p++;
+ }
+ }
+ if(p==2)
+ {
+ printf("yes");
+   }
+ else
+ {
+  printf("no");
+   }
+}
