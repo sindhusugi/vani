5870812f0e499e36c838f1a9b8094d83bde9cde7
sindhusugi <30830093+sindhusugi@users.noreply.github.com>
2018-03-15 16:09:33 +0530

    Create 61string.c

---
+++ 61string.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10];
+int i,n;
+printf(" ");
+scanf("%s",&a);
+printf(" ");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("%c",a[i]);
+}
+return 0;
+}
